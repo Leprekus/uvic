d8825ac42a909204f00d425b8b0b853a2d6f10dc
Leprekus <leprekus@yahoo.com>
2023-10-12 08:01:51 -0700

    create ass 4 folder

---
+++ CSC/115/assignments/assignment-4/A4Exercises.java
@@ -0,0 +1,31 @@
+public class A4Exercises {
+	
+	/*
+	 * Purpose: determine if the stack of plates has been
+	 *          stacked correctly (ie. there is never a plate
+	 *          on top of a smaller plate)
+	 * Parameters: Stack<Plate> s - a stack of plates
+	 * Returns: boolean - true if the plates are stacked correctly
+	 *                    false otherwise
+	 * Post-condition: the contents of s are not modified
+	 */
+	public static boolean stackedCorrectly(Stack<Plate> s) {
+		Stack<Plate> temp = new A4Stack<Plate>();
+		
+		return false; // so it compiles
+	}
+	
+	/*
+	 * Purpose: insert p into the correct location in the
+	 *          stack such that there are no smaller plates 
+	 *          below p and no larger plates above p
+	 * Parameters: Stack<Plate> s - a stack of plates
+	 *             Plate p - the plate to insert into s
+	 * Returns: void - nothing
+	 * Pre-condition: plates in s have been stacked correctly
+	 */
+	public static void insertPlate(Stack<Plate> s, Plate p) {
+		Stack<Plate> temp = new A4Stack<Plate>();
+		
+	}
+}
---
+++ CSC/115/assignments/assignment-4/A4Stack.java
@@ -0,0 +1,8 @@
+public class A4Stack<T> implements Stack<T> {
+	private A4Node<T> head;
+
+	public A4Stack() {
+		head = null;
+	}
+	
+}
