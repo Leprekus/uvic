c7a75e989d12c8955f1079769480eb7cddeda724
Leprekus <leprekus@yahoo.com>
2023-10-19 07:58:41 -0700

    add ass-5 files

---
+++ CSC/115/assignments/assignment-5/A5Exercises.java
@@ -0,0 +1,116 @@
+public class A5Exercises {
+
+	// PART 1
+
+	/*
+	 * Purpose: get the sum of the even values in the array
+	 * Parameters: int[] arr
+	 * Returns: int - the sum number of even-valued elements
+	 * Post-condition - the array contents are unchanged
+	 */
+	public static int sumEven(int[] arr) {
+		// TODO: implement this
+		return -1; // so it compiles
+	}
+	
+	/*
+	 * Purpose: add x to all values in the given array
+	 * Parameters: int[] array - the array to modify
+	 *             int x - the value to add to all elements
+	 * Returns: void - nothing
+	 */
+	public static void addXToAll(int[] array, int x) {
+		// TODO: implement this
+	}
+	
+	/*
+	 * Purpose: get the maximum value found in the array
+	 * Parameters: int[] array - the array to search
+	 * Returns: int - maximum value found in the array
+	 *                or -1 if the array is empty
+	 * Post-condition - the array contents are unchanged
+	 */
+	public static int getMaximum(int[] array) {
+		// TODO: implement this
+		return -1; // so it compiles
+	}
+	
+	/*
+	 * Purpose: determines if there is at least one occurrence of x
+	 *          after the occurrence of y in the array
+	 * Parameters: int[] array - the array to search
+	 *             int x - the value that must come second
+	 *             int y - the value that must come first
+	 * Returns: boolean - true if there is at least one occurrence
+	 *                    of x after the occurrence of y
+	 */
+	public static boolean comesAfter(int[] array, int x, int y) {
+		// TODO: implement this
+		return false; // so it compiles
+	}
+	
+
+
+	/////////////
+	// PART II //
+	/////////////
+	
+	/*
+	 * Purpose: get the total number of beads in s
+	 * Parameters: Stack<BeadBox> s - the stack of bead boxes
+	 * Returns: int - the total number of beads
+	 * Post-condition: s is not modified
+	 */
+	public static int beadsCount(Stack<BeadBox> s) {
+		// TODO: implement this
+		return -1; // so it compiles
+	}
+
+	/*
+	 * Purpose: get the total number of bead boxes in s
+	 * Parameters: Stack<BeadBox> s - the stack of bead boxes
+	 * Returns: int - the total number of bead boxes
+	 * Post-condition: s is not modified
+	 */
+	public static int beadBoxesCount(Stack<BeadBox> s) {
+		// TODO: implement this
+		return -1; // so it compiles
+	}
+	
+	/*
+	 * Purpose: get the total weight of all bead boxes
+	 * Parameters: Stack<BeadBox> s - the stack of bead boxes
+	 * Returns: double - the total weight of all boxes
+	 * Post-condition: s is not modified
+	 */
+	public static double totalWeight(Stack<BeadBox> s) {
+		// TODO: implement this
+		return -1.0; // so it compiles
+	}
+
+	/*
+	 * Purpose: get the average weight of the bead boxes in s
+	 * Parameters: Stack<BeadBox> s - the stack of bead boxes
+	 * Returns: double - the average weight of the bead boxes
+	 *                   0.0 if there are no bead boxes in s
+	 * Post-condition: s is not modified
+	 */
+	public static double averageWeight(Stack<BeadBox> s) {
+		// TODO: implement this
+		return -1.0; // so it compiles
+	}
+
+	/*
+	 * Purpose: determine the bead boxes in s are stacked correctly
+	 *          (ie. there is never a bead box stacked on top of 
+	 *               another bead box that weighs LESS than it does)
+	 * Parameters: Stack<BeadBox> s - the stack of bead boxes
+	 * Returns: boolean - true if bead boxes in s are stacked correctly
+	 * Post-condition: s is not modified
+	 */
+	public static boolean stackedCorrectly(Stack<BeadBox> s) {
+		// TODO: implement this
+		return false; // so it compiles
+	}
+
+}
