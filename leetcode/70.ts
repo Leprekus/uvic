67d589438c5cf27cac85efb6b8a94b282623c985
root <root@DESKTOP-C1S1VV7.>
2025-05-11 14:53:44 -0700

    implement constant space to calculate BIG NUMS

--- leetcode/70.ts
+++ leetcode/70.ts
@@ -1,22 +1,26 @@
 function climbStairs(n: number): number {
-   const memo = {}; 
-   let result = 0;
+   //const memo = {}; 
+   let result = 0, prev = 0, curr = 0;
    for(let i = 0; i < n + 1; i++){
 
       /*
       if(i <= 1) result = 1;
       else result = memo[i - 1] + memo[i - 2];
+      memo[i] = result;
+      return memo[n];
       */
 
       result = 
          (1 & (i <= 1) as unknown as number) | 
-         (memo[i -1] + memo[i - 2] & ~(i <= 1));
+         (prev + curr & ~(i <= 1));
       
-      memo[i] = result;
+      prev = curr;
+      curr = result;
    }
-   return memo[n];
+   return result;
 
 };
 
 
+//console.log(climbStairs(5), climbStairs(10));
 console.log(climbStairs(999999999));
