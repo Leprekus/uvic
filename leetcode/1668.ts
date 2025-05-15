0d14b122c6d82be6e7e09228b08815b76603f304
root <root@DESKTOP-C1S1VV7.>
2025-05-15 07:36:04 -0700

    finished

--- leetcode/1668.ts
+++ leetcode/1668.ts
@@ -1,30 +1,10 @@
 function maxRepeating(sequence: string, word: string): number {
-
 	
-	//  rows represent sequence
-	//  cols represent word
-	//  so if word matches at any given part of sequence there will be
-	//  a match at A[i][j]
-	//  --------------
-	// \		\
-	// \		\
-	// \		\
-	// \		\
-	// \		\
-	// -------------
-	const m = sequence.length, n = word.length;
-	let count = 0, substr = 0;
-
-	const A = new Array(m).fill(new Array(n).fill(0));
-	A.fill(new Uint16Array(n));
-
-	for(let i = 1; i < m; i++){
-		for(let j = 1; j < n; i++){
-			if(sequence[i] === word[j]) A[i][j] = A[i - 1][j - 1] + 1;
-			
-			if(count === word.length) substr++, count = 0;
-		}
-		
-	}
+    	let count = 0, w = word;
+        while(sequence.includes(w)){
+            w += word;
+            count++;
+        }
+        return count;
 
 };
