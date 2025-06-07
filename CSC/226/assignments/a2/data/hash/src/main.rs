914101faf1c7828fb04b10623eb2bd59bdce4473
Leprekus <leprekus@yahoo.com>
2025-06-06 23:56:08 -0700

    implement csv writing

--- CSC/226/assignments/a2/data/hash/src/main.rs
+++ CSC/226/assignments/a2/data/hash/src/main.rs
@@ -1,4 +1,7 @@
 use std::io::BufRead;
+use std::fs::OpenOptions;
+use std::io::{Write};
+#[derive(Debug)]
 enum Method {
     Linear,
     Double
@@ -9,17 +12,23 @@ struct Hash {
     size: usize,
     alpha: usize,
     data: Vec<i32>,
+    records: Vec<String>
 }
 
 
 impl Hash {
     pub fn new(capacity: usize, method: Method) -> Self {
+        let headers = String::from("item number, key, status, load factor");
+        let mut records = Vec::new();
+        records.push(String::from(format!("{:#?},,,", method)));
+        records.push(headers);
         Self{
             method,
             capacity,
             size: 0,
             alpha: 0,
-            data: vec![-1; capacity]
+            data: vec![-1; capacity],
+            records, 
         }
     }
 
@@ -64,10 +73,23 @@ impl Hash {
         }
         let mut i = 0;
         let mut idx = self.h(k.clone(), i);
+
         while self.data[idx] != -1 {
+            //if we push here it was a miss
+            self.records.push(
+                String::from(
+                    format!("{}, {}, {}, {}", self.size, idx, "miss", (self.size as f64)/(self.capacity as f64))
+                )
+            );
             i += 1;
             idx = self.h(k.clone(), i);
         }
+        self.records.push(
+                String::from(
+                    format!("{}, {}, {}, {}", self.size, idx, "hit", (self.size as f64)/(self.capacity as f64))
+                )
+            );
+
         self.data[idx as usize] = self.h(k, i) as i32;
         self.size += 1;
         self.alpha = self.size.wrapping_div(self.capacity);
@@ -78,6 +100,7 @@ impl Hash {
     fn clear(&mut self) {
         self.data = vec![-1; self.capacity];
         self.size = 0;
+        self.records = Vec::new();
     }
 
 }
@@ -98,6 +121,27 @@ impl Reader {
         }
     }
 }
+
+struct Writer {}
+impl Writer {
+    //https://stackoverflow.com/questions/31192956/whats-the-de-facto-way-of-reading-and-writing-files-in-rust-1-x
+    fn write(path: String, data: Vec<String>) {
+        //println!("records\n{:?}", data);
+        let data: Vec<u8> = data
+                .iter()
+                .flat_map(|s| s.as_bytes().iter().copied().chain(std::iter::once(b'\n')))
+                .collect();
+        //std::fs::write(path, data).expect("Unable to write file")
+        let mut file = OpenOptions::new()
+            .create(true)      // create if it doesn't exist
+            .append(true)      // append if it does
+            .open(path)
+            .expect("unable to open / create file");
+
+        file.write_all(&data).expect("unable to write to file");
+    
+    }
+}
 fn main(){
 
     
@@ -106,13 +150,14 @@ fn main(){
     Reader::read_stdin(100_000, |str| {
 
         if hash_linear.size == hash_linear.capacity {
+            Writer::write(String::from("../records_linear.csv"), hash_linear.records.clone());
             hash_linear.clear();
         } else { 
-            println!("clearing");
             hash_linear.insert(str.clone()); 
         }
 
         if hash_double.size == hash_linear.capacity {
+            Writer::write(String::from("../records_double.csv"), hash_linear.records.clone());
             hash_double.clear();
         } else { 
             hash_double.insert(str); 
