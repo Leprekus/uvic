119a0869c4a952a44bb02762bf8b3296177c93bf
Leprekus <leprekus@yahoo.com>
2025-06-14 15:28:23 -0700

    implement macro to extract tokens

--- CSC/226/assignments/a3/src/utils/graphBuilder.c
+++ CSC/226/assignments/a3/src/utils/graphBuilder.c
@@ -1,17 +1,42 @@
 #include "../include/utils/reader.h"
 #include "../include/utils/graphBuilder.h"
 
+#define EXTRACT_TOKEN(tokens, buff, i, j, len)                 \
+	for((i) = (i); (i) < (len); (i)++){\
+		if( (tokens)[(i)] == '\0' ) { \
+			while((i) < (len) - 1 && (tokens)[++(i)] == '\0'){} \
+     			break;\
+		}\
+     		else {\
+			(buff)[(j)++] = (tokens)[(i)];\
+		}\
+	} (buff)[(j)] = '\0';\
 void processLine(const char *, size_t);
-void processLine(const char *line, size_t _){
+void processLine(const char *line, size_t lineLen){
 
-	(void)_;
-	size_t tokensLen = 128;
 	/* tokenized with delimiters: \t, - 
 	 * token form: start \0 weight \0 end \0 weight
 	 * */
-	char tokens[tokensLen]; tokenizeStr(line, tokens, tokensLen, "\t-", 2);
+	char tokens[lineLen]; tokenizeStr(line, tokens, lineLen, "\t-", 2);
 	
-	for(size_t i = 0; i < tokensLen; i++)
+	char key[lineLen]; safeStrcpy(tokens, key, lineLen);
+	char value[lineLen];
+	size_t i = 0; //indexes tokens
+	size_t j = 0; //indexes key / value
+	
+	EXTRACT_TOKEN(tokens, key, i, j, lineLen) j = 0;
+	EXTRACT_TOKEN(tokens, value, i, j, lineLen) j = 0;
+	printf("k1: %s\n", key);
+	printf("v1: %s\n", value);
+
+	EXTRACT_TOKEN(tokens, key, i, j, lineLen) j = 0;
+	EXTRACT_TOKEN(tokens, value, i, j, lineLen) j = 0;
+	printf("k2: %s\n", key);
+	printf("v2: %s\n", value);
+
+	
+
+	for(size_t i = 0; i < lineLen; i++)
 		if(tokens[i] != '\0') putchar(tokens[i]);
 	putchar('\n');
 
