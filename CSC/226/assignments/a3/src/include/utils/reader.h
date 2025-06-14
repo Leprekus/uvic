2deeb65c98a903b916403910b4fcb4415f0bd26c
Leprekus <leprekus@yahoo.com>
2025-06-14 13:44:42 -0700

    fix callback dfn

--- CSC/226/assignments/a3/src/include/utils/reader.h
+++ CSC/226/assignments/a3/src/include/utils/reader.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void (*callback)(char *, size_t);
+typedef void (*callback)(const char *, size_t);
 void parseFile(const char *path, callback cb);
 void tokenizeStr(const char *str, char *buff, size_t buffLen, char *del, size_t delLen);
 
