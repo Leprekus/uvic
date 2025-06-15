87cd3d3c79402e6c38b64f619bcf01379d722428
Leprekus <leprekus@yahoo.com>
2025-06-15 13:28:41 -0700

    move extract token to reader.h

--- CSC/226/assignments/a3/src/include/utils/reader.h
+++ CSC/226/assignments/a3/src/include/utils/reader.h
@@ -3,6 +3,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * usage: the ith call to EXTRACT_TOKEN retrieves the ith tokens
+ * takes a tokenized buffer, a buffer to copy the ith token to, and counters i, j
+ * i: indexes the tokens buffer
+ * j: indexes the buffer to store the token
+ *
+ * */
+#define EXTRACT_TOKEN(tokens, buff, i, j, len)                 \
+	for((i) = (i); (i) < (len); (i)++){\
+		if( (tokens)[(i)] == '\0' ) { \
+			while((i) < (len) - 1 && (tokens)[++(i)] == '\0'){} \
+     			break;\
+		}\
+     		else {\
+			(buff)[(j)++] = (tokens)[(i)];\
+		}\
+	} (buff)[(j)] = '\0';\
+
+
 typedef void (*callback)(const char *, size_t);
 void parseFile(const char *path, size_t lineLen, callback cb);
 void tokenizeStr(const char *str, char *buff, size_t buffLen, char *del, size_t delLen);
