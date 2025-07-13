aa087081447ab9bf95011a72c1de581f5ea80d86
leprekus <leprekus@yahoo.com>
2025-07-12 17:50:31 -0700

    create graph and edge classes

--- CSC/dsa-II/graph/dijkstra.ts
+++ CSC/dsa-II/graph/dijkstra.ts
@@ -1,11 +1,25 @@
 type Items<T> = Array<T>
-class Dijkstra<Items> {
-    items: Items;
-    heap: Items;
-    constructor(items: Items) {
-        this.items = items;
-        this.heap = [] as Items;
-    }
-
-    private heapify() {}
+type Edges = Edge[];
+interface Vertex {};
+class Edge {
 }
+
+class Graph {
+    constructor(E: Edges){
+    }
+}
+class Dijkstra<Items> {
+    G: Graph;
+    s: Vertex;
+    constructor(G: Graph, s: Vertex) {
+        this.G = G;
+        this.s = s;
+    }
+    private heapify() {}
+    public popMin() {}
+}
+
+const g: Graph = {};
+const s: Vertex = {};
+const d = new Dijkstra(g, s);
+
