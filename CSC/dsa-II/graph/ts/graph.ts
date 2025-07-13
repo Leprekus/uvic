05b37d9c2bcd557a8e2f00b06897a1856790ec64
leprekus <leprekus@yahoo.com>
2025-07-12 18:57:45 -0700

    construct edges and test them

--- CSC/dsa-II/graph/ts/graph.ts
+++ CSC/dsa-II/graph/ts/graph.ts
@@ -1,19 +1,27 @@
 import Edge, { type Edges } from "./edge";
 
-type Items<T> = Array<[T, T, number]>;
+export type GraphItems<T> = Array<[T, T, number]>;
 export default class Graph<T> {
     edges: Map<T, Edges<T>>;
-    constructor(items: Items<T>){
+    /*
+     * accepts an array containing elements of the form [source, adjacent, weight]
+     * */
+    constructor(items: GraphItems<T>){
         this.edges = this.constructEdges(items); 
     }
-    private constructEdges(items: Items<T>): Map<T, Edges<T>>{
+    private constructEdges(items: GraphItems<T>): Map<T, Edges<T>>{
+
         const ret = new Map<T, Edges<T>>();
         for(const [u, v, w] of items){
+
             const edges = ret.get(u) || [];
             edges.push(new Edge<T>(u, v, w));
             ret.set(u, edges);
 
         }
         return ret;
+    }
+    public adjacent(u: T): Edges<T>{
+        return this.edges.get(u) || [];
     }
 }
