05430bf519634312083fc821cb7478aedc691904
leprekus <leprekus@yahoo.com>
2025-07-26 19:16:14 -0700

    define graph fn and boundary fn

--- CSC/226/assignments/a5/seam-carving/src/main.rs
+++ CSC/226/assignments/a5/seam-carving/src/main.rs
@@ -1,8 +1,8 @@
 use std::path::{ self, Path, PathBuf };
 use std::io::Cursor;
-use image::{ ImageReader };
+use image::{ GenericImageView, ImageReader };
 
-fn get_sd(values: Vec<u8>) -> f64 {
+fn get_sd(values: &Vec<u8>) -> f64 {
     let (sum, count) = values.iter()
         .fold((0u32, 0u32), |(sum, count), &v| (sum + v as u32, count + 1) );
     let mean = sum as f64 / count as f64;
@@ -14,6 +14,20 @@ fn get_sd(values: Vec<u8>) -> f64 {
         ).sqrt();
     sd
 }
+
+fn boundary() -> i32 {
+    1
+}
+type Graph = Vec<Vec<i32>>;
+fn create_graph(pixels : &Vec<u8>, width: u32, height: u32) -> Graph{
+    let mut g: Graph = vec![ vec![0; width as usize]; height as usize];
+    for u in 0..height {
+        for v in 0..width {
+            g[u as usize][v as usize] = boundary();
+        }
+    }
+    g
+}
 fn main() {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.pop();
@@ -24,7 +38,9 @@ fn main() {
     let img = ImageReader::open(src_path.as_path())
        .unwrap().decode().unwrap();
     let img = img.to_luma8();
-    let sd = get_sd(img.pixels().map(|p| p[0]).collect()); 
+    let pixels: Vec<u8> = img.pixels().map(|p| p[0]).collect();
+    let g = create_graph(&pixels, img.width(), img.height());
+    let sd = get_sd(&pixels); 
     let _ = img.save(dest_path.as_path());
     println!("sd {:?}", sd);
 }
