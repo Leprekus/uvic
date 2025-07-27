8d26e5046abcc39c540e18bcdd359b1583512617
leprekus <leprekus@yahoo.com>
2025-07-26 18:02:21 -0700

    compute sd

--- CSC/226/assignments/a5/seam-carving/src/main.rs
+++ CSC/226/assignments/a5/seam-carving/src/main.rs
@@ -1,12 +1,30 @@
 use std::path::{ self, Path, PathBuf };
-use std::fs;
+use std::io::Cursor;
 use image::{ ImageReader };
 
+fn get_sd(values: Vec<u8>) -> f64 {
+    let (sum, count) = values.iter()
+        .fold((0u32, 0u32), |(sum, count), &v| (sum + v as u32, count + 1) );
+    let mean = sum as f64 / count as f64;
+    let sd: f64 = (
+            values.iter().fold( 0f64, | sum, &xi| 
+                sum + (xi as f64 - mean).powi(2)
+            ) 
+            / (count as f64 - 1.0)
+        ).sqrt();
+    sd
+}
 fn main() {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.pop();
-    path = path.join("la-primavera-boticelli.jpg");
-    let img = ImageReader::open(path.as_path())
+
+    let src_path = path.join("la-primavera-boticelli.jpg");
+    let dest_path = path.join("output.jpg");
+
+    let img = ImageReader::open(src_path.as_path())
        .unwrap().decode().unwrap();
-    println!("{:?}", img);
+    let img = img.to_luma8();
+    let sd = get_sd(img.pixels().map(|p| p[0]).collect()); 
+    let _ = img.save(dest_path.as_path());
+    println!("sd {:?}", sd);
 }
