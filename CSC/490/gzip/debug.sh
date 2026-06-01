fd45fa32787008d6cf7b183733efa86dea2ff172
leprekus <leprekus@yahoo.com>
2026-06-01 08:03:00 -0700

    list compression

--- CSC/490/gzip/debug.sh
+++ CSC/490/gzip/debug.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 make all
-fname=filename(basename $1)
+fname=$(basename "$1")
 ./main < $1 > "./out/$fname.gz" # create empty gzip using main
 echo "=== gzstat.py ===" # validate with gzstat
 python3 gzstat.py --print-block-codes --decode-blocks < "./out/$fname.gz"
 echo "=== gzip -d ===" # decompress with gzip
+gzip -l "./out/$fname.gz"  
 gzip -dc "./out/$fname.gz"
 echo "=== xxd ===" # view hex output with xxd
 cat "./out/$fname.gz" | xxd
