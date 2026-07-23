4362d536e9a4928745fb1c7ec8c218e3c63b3e16
leprekus <leprekus@yahoo.com>
2026-07-22 17:19:36 -0700

    impl: writing compressed block to bitstream (WORKING)

--- CSC/490/video-compression/uvid/utils.cpp
+++ CSC/490/video-compression/uvid/utils.cpp
@@ -41,36 +41,63 @@ void print(const Macroblock &mb, std::string tag) {
       std::cerr << static_cast<int>(std::get<0>(mb.vect)) << " " << static_cast<int>(std::get<1>(mb.vect)) << " " << static_cast<int>(std::get<2>(mb.vect)) << std::endl;
    }
 }
-
-void write_zero(const int run, OutputBitStream &stream){
+/*
+ * 00         - end of stream
+ * 01         - 1+ repetitions
+ * 01 00      - run of 1
+ * 01 10      - run of 2
+ * 01 110   1 - run of 3  
+ * 01 1110  00 - run of 4  
+ * 01 1110  10 - run of 6
+ * 01 11110 000 - run of 8
+ * RUN LENGTH   VALUE
+ * */
+void write_zero( u32 run, OutputBitStream &stream){
    // push zero for no delta
-   stream.push_bit(0);
+   /*stream.push_bit(0);*/ std::cerr << "0";
    if(run) {
-      stream.push_bit(1); // push one for at least one repetition 
+      /*stream.push_bit(1);*/ std::cerr << "1"; // push one for at least one repetition 
       if(run == 1) { // handle a run of 1
-         stream.push_bit(0);
-         stream.push_bit(0);
+         /*stream.push_bit(0);*/ std::cerr << "0";
+         /*stream.push_bit(0);*/ std::cerr << "0";
       } else if(run == 2) { // handle a run of 2
-         stream.push_bit(1);
-         stream.push_bit(0);
+         /*stream.push_bit(1);*/ std::cerr << "1";
+         /*stream.push_bit(0);*/ std::cerr << "0";
       } else { // handle a run of 3+
-         int tmp = run; // get bits count
-         while(tmp > 0) { tmp >>= 1; stream.push_bit(1); }
-         stream.push_bit(0);
+         // output length in unary
+         int size = std::bit_width(run);
+         for(int i = 0; i < size; i++)
+               /*stream.push_bit(1);*/ std::cerr << "1"; 
+         // end of unary length
+         /*stream.push_bit(0);*/ std::cerr << "0";
+
+         // output value except MSB  
+         assert(size >= 2);
+         for(int i = size - 2; i >= 0; i--) {
+            u32 bit = run & (1U << i);
+             /*stream.push_bit(bit);*/ std::cerr << (bit ? "1" : "0");
+         }
+         
       }
    } else { // push zero for no repetitions (end of stream)
-      stream.push_bit(0);
+      /*stream.push_bit(0);*/ std::cerr << "0";
    }
+   std::cerr << "\n";
    
 }
-void write_coefficient(const double value, u32 run, OutputBitStream &stream){}
-void write_bitsream(const double value, u32 run, OutputBitStream &stream) {
+void write_coefficient(const double value, int run, OutputBitStream &stream){
+   std::cerr << "coefficient : " << value << " run " << run << "\n";
+}
+void write_bitstream(const auto value, const u32 run, OutputBitStream &stream) {
    bool value_is_zero = value == 0;
    
-   if(value_is_zero)
+   std::cerr << "value: " << value << " run : " << run << "\n";
+   if(value_is_zero) {
       write_zero(run, stream); 
-   else 
-      write_coefficient(value, run, stream);
+   } else {
+      //write_coefficient(value, run, stream);
+   }
+   std::cerr << "\n";
    
 }
 
@@ -109,30 +136,30 @@ void bitstream_to_compressed_mb(const Macroblock &mb, InputBitStream &stream) {
 
 /* get run q*/
 template <typename EigenMatrix>
-void _compressed_block_to_bistream(OutputBitStream &stream, const EigenMatrix &M,  auto &traversal) {
+void _compressed_block_to_bitstream(OutputBitStream &stream, const EigenMatrix &M,  auto &traversal) {
    assert(M.cols() == M.rows());
    assert(M.cols() * M.cols() == traversal.size());
    size_t len = M.size();
    size_t cols = M.cols();
 
    auto curr = M(traversal.front().first, traversal.front().second);
-   int run = 0;
+   u32 run = 0;
    for(auto [x, y]: traversal) {
       if(curr == M(x, y))
          run++;
       else {
-         std::cerr << "item: " << curr << " run: " << run << "\n";
+         write_bitstream(curr, run, stream);
          run = 1;
          curr = M(x, y);
       }
    }
    if(run) {
-      std::cerr << "item: " << curr << " run: " << run << "\n";
+      write_bitstream(curr, run, stream);
    }
 
    
 }
-void compressed_mb_to_bistream(const Macroblock &mb, OutputBitStream &stream) {
+void compressed_mb_to_bitstream(const Macroblock &mb, OutputBitStream &stream) {
    auto [x, y, idx] = mb.vect;
    bool row_scan = x != 0 && y == 0;
    bool col_scan = x == 0 && y != 0;
@@ -151,8 +178,7 @@ void compressed_mb_to_bistream(const Macroblock &mb, OutputBitStream &stream) {
    }
    auto &ref_8x8 = *ptr_8x8;
    auto &ref_16x16 = *ptr_16x16;
-   std::cerr << "col_scan: " << col_scan  << " row " << row_scan << " col " << col_scan << " zig " << zig_scan << "\n";
-   //_compressed_block_to_bistream(stream, mb.Y, ref_16x16);
-   _compressed_block_to_bistream(stream, mb.Cb, ref_8x8);
+   //_compressed_block_to_bitstream(stream, mb.Y, ref_16x16);
+   _compressed_block_to_bitstream(stream, mb.Cb, ref_8x8);
    //_compress_block(mb.Cr, stream);
 }
